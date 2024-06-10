a5967ed8bfaa8cdb09035f6feb29e10fa055a1e3
unknown <jkktg001@gmail.com>
2024-06-10 23:41:20 +0530

    2024/06/10

---
+++ src/system_modules/sys_company_manager/endpoints/edit-company-user.ts
@@ -0,0 +1,72 @@
+import e from "express";
+import reserved_roles from "../../../lib/reserved_roles";
+import { check_user_permission } from "../../../modules/app_manager";
+import UserRole from "../../../schemas/company/user_permission_schema";
+import User from "../../../schemas/company/user_schema";
+import { hash } from "bcrypt";
+
+interface CompanyUserBody {
+    _id: string,
+    first_name?: string,
+    last_name?: string,
+    email?: string,
+    password?: string,
+    role_id?: string,
+}
+
+export default async function edit_company_user(req: any, res: any) {
+    const user = req.user;
+    const { _id, first_name, last_name, email, password, role_id } = req.body as CompanyUserBody;
+
+    // check if the user has the permission to create a user role
+    if (!check_user_permission(user, 'create_user_role') && !check_user_permission(user, 'company-admin')) {
+        return res.status(403).json({ message: 'No permissions' });
+    };
+
+    // get the user
+    const editing_user = await User.findOne({ _id });
+    if (!editing_user) {
+        return res.status(400).json({ message: 'User not found' });
+    }
+
+    let is_edited = false;
+    if (first_name && first_name !== editing_user.first_name) {
+        editing_user.first_name = first_name;
+        is_edited = true;
+    }
+    if (last_name && last_name !== editing_user.last_name) {
+        editing_user.last_name = last_name;
+        is_edited = true;
+    }
+
+    if (email && email !== editing_user.email) {
+        editing_user.email = email;
+        is_edited = true;
+    }
+
+    if (password) {
+        if (password.length < 6) {
+            return res.status(400).json({ message: 'Password must be at least 6 characters long' });
+        }
+
+        const hashed_password = await hash(password, 10);
+        editing_user.password = hashed_password;
+        is_edited = true;
+    }
+
+    if (role_id && role_id !== editing_user.role.toString()) {
+        const userRole = await UserRole.findById(role_id);
+        if (!userRole) {
+            return res.status(400).json({ message: 'Role not found' });
+        }
+        editing_user.role = userRole._id;
+        is_edited = true;
+    }
+
+    if (is_edited) {
+        await editing_user.save();
+        return res.status(200).json(editing_user);
+    } else {
+        return res.status(400).json({ message: 'No changes' });
+    }
+}
--- src/system_modules/sys_company_manager/main.ts
+++ src/system_modules/sys_company_manager/main.ts
@@ -3,13 +3,22 @@
  *      - Get list a of companies
  *      - Register a company
  *      - Add a user to a company
- *  
+ *      - Update company details
+ *      - Delete a company
+ *      - Get user roles
+ *      - Get list of users in a company
+ *      - Edit a user in a company
  */
 import { register_api_endpoint, register_user_permissions } from "../../modules/app_manager";
 import config from "./config";
 import create_new_company from "./endpoints/create-company";
 import create_new_company_user from "./endpoints/create-company-user";
 import get_company_list from "./endpoints/get-list-of-companies";
+import update_company from "./endpoints/edit-company";
+import delete_company from "./endpoints/delete-company";
+import get_user_role_list from "./endpoints/grt-user-rolls";
+import get_user_list from "./endpoints/get-list-of-company-users";
+import edit_company_user from "./endpoints/edit-company-user";
 
 /** Init the module */
 export function init_module(){
@@ -56,5 +65,40 @@ function endpoints() {
         is_protected: true,
         method: "POST",
         handler: create_new_company_user
+    });
+
+    register_api_endpoint({
+        route: `${route_prefix}${company_prefix}/update`,
+        is_protected: true,
+        method: "POST",
+        handler: update_company
+    });
+
+    register_api_endpoint({
+        route: `${route_prefix}${company_prefix}/delete`,
+        is_protected: true,
+        method: "POST",  // Method is POST for deletion
+        handler: delete_company
+    });
+
+    register_api_endpoint({
+        route: `${route_prefix}${company_prefix}/get-user-rolls`,
+        is_protected: true,
+        method: "GET",  
+        handler: get_user_role_list
+    });
+
+    register_api_endpoint({
+        route: `${route_prefix}${company_prefix}/get-user-list`,
+        is_protected: true,
+        method: "POST",  
+        handler: get_user_list
+    });
+
+    register_api_endpoint({
+        route: `${route_prefix}${company_prefix}/edit-user`,
+        is_protected: true,
+        method: "POST",  
+        handler: edit_company_user
     });
 }
